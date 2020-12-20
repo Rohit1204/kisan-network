23bdeca1562385e42ae193940b68333496be9934
rohit1204 <rohitahuja725@gmail.com>
2020-12-20 20:06:36 +0530

    added  show data

---
+++ src/components/show-contacts/HorizontalForm.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+import axios from "axios";
+
+class HorizontalTable extends PureComponent {
+
+
+render(){
+    return(
+        <div> hello </div>
+    );
+}
+
+}
+
+export default HorizontalTable;
