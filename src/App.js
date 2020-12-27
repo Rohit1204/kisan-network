4bda93b47764fbef00b51d97ec11f9548fae2ac8
rohit1204 <rohitahuja725@gmail.com>
2020-12-27 18:28:59 +0530

    added header'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import PrimarySearchbar from "../src/components/header/index"
 import Showcontacts from "../src/components/show-contacts/HorizontalForm"
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-    <Showcontacts />
+      <PrimarySearchbar/>
+      <Switch>
+        <Route exact path='/' component={Showcontacts}/>
+        {/* <Route  path='/User-Contact/:id' component={UserContact}/>
+        <Route  path='/otp-sent/:id' component={OtpSend}/> */}
+        {/* <Route component={NoMatch}/> */}
+      </Switch>
     </div>
   );
 }
